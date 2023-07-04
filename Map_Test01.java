fecb977273b283d16073954e0e554fc86aea557a
shubham barude <shubhambarude789@gmail.com>
2023-07-04 19:14:03 +0530

    it is collection program

---
+++ Map_Test01.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import jAVA8Feature.Employee;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Map_Test01
+{
+    public static void main(String[] args) {
+
+
+    }
+}
